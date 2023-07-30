74715a21cc0e0b8e7ffa0c8d8a5c672cf6d60d70
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-07-30 11:36:00 -0400

    added type hints and changed unclear variable names

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PIL import Image # type: ignore
 import numpy as np
+from numpy.typing import NDArray
 import shutil
 
 LOAD_IMG_WIDTH = shutil.get_terminal_size().columns + 2
@@ -21,10 +22,10 @@ FILTER_PAIRS = [
     (UNDERSCORE_FILTER, '_')
 ]
 
-def change_pixel_to_gray_scale(pixel):
-    red = pixel[0]
-    green = pixel[1]
-    blue = pixel[2]
+def change_pixel_to_gray_scale(pixel: NDArray[np.uint8]):
+    red: int = pixel[0]
+    green: int = pixel[1]
+    blue: int = pixel[2]
 
     # Apply the following formula to find the greyscale value for the pixel
     gray_scale_value = 0.2989 * red + 0.5870 * green + 0.1140 * blue
@@ -32,14 +33,15 @@ def change_pixel_to_gray_scale(pixel):
     # Change each color value in the pixel to the calculated greyscale value
     pixel[:] = int(gray_scale_value)
 
-def print_filter_output(input_data):
-    min_filer_output = 150
-    min_filer_string = ' '
+def print_filter_output(img_region: NDArray[np.uint8]):
+    min_filer_output: int = 150
+    min_filer_string: str = ' '
 
     # Finds the filter that maps the best and saves it's conesponding character
+    # note: filter_matrix is a numpy array of type int32
     for filter_matrix, mapped_string in FILTER_PAIRS:
-        multiplied_output = input_data * filter_matrix
-        filter_output = np.sum(multiplied_output)
+        multiplied_output = img_region * filter_matrix
+        filter_output: int = np.sum(multiplied_output)
 
         if filter_output < min_filer_output:
             min_filer_output = filter_output
@@ -47,25 +49,25 @@ def print_filter_output(input_data):
     
     print(min_filer_string, end='')
 
-def print_img(img_original):
+def print_img(img: NDArray[np.uint8]):
     for i in range(1, LOAD_IMG_HEIGHT - 1):
         for j in range(1, LOAD_IMG_WIDTH - 1):
             # Loads the pixel at location i, j and the along with the 8
             # surrounding it
-            input_data = img_original[i - 1: i + 2, j - 1: j + 2]
+            img_region = img[i - 1: i + 2, j - 1: j + 2]
 
             # Load the pixels as a 3 * 3 array (one int representing each pixel)
-            input_data = input_data[:,:,0:1]
-            input_data = input_data.reshape((3,3))
+            img_region = img_region[:,:,0:1]
+            img_region = img_region.reshape((3,3))
 
-            print_filter_output(input_data)
+            print_filter_output(img_region)
 
 
-def main():
+def main() -> None:
     # Loads image as 80 * 80 pixels and saves it in a numpy array
     img_raw = Image.open('github.png')
     img_raw = img_raw.resize((LOAD_IMG_WIDTH, LOAD_IMG_HEIGHT))
-    img = np.array(img_raw)
+    img: NDArray[np.uint8] = np.array(img_raw)
 
     # Removes alpha channel from an image if present
     if img_raw.mode == 'RGBA':
