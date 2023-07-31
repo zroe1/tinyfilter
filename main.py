ae8cd472a0dcd38eeae9c254286d80a84265f099
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-07-30 22:15:36 -0400

    added docstrings and updated comments

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+"""Loads an image and prints it to the console as ASCII characters.
+
+This file applies convolution to an image to detect small features in an image
+that translate directly to ASCII characters. The result of the operation is
+printed to the console as it occurs.
+"""
 from PIL import Image  # type: ignore
 import numpy as np
 from numpy.typing import NDArray
@@ -24,25 +30,34 @@ FILTER_PAIRS = [
 ]
 
 
-def change_pixel_to_gray_scale(pixel: NDArray[np.uint8]):
+def change_pixel_to_gray_scale(pixel: NDArray[np.uint8]) -> None:
+    """Modifies a pixel in place to it's grayscale equivalent.
+
+    Args:
+        pixel: A 1D numpy array of length 3 representing an RGB pixel
+    """
     red: int = pixel[0]
     green: int = pixel[1]
     blue: int = pixel[2]
 
-    # Apply the following formula to find the greyscale value for the pixel
     gray_scale_value = 0.2989 * red + 0.5870 * green + 0.1140 * blue
 
-    # Change each color value in the pixel to the calculated greyscale value
     pixel[:] = int(gray_scale_value)
 
 
-def print_filter_output(img_region: NDArray[np.uint8]):
+def print_filter_output(img_region: NDArray[np.uint8]) -> None:
+    """Prints the character with the most present filter in img_region.
+
+    Args:
+        img_region: a 3 * 3 numpy array. Each number corresponds to a greyscale
+          pixel in the original image.
+    """
     min_filer_output: int = 150
     min_filer_string: str = " "
 
-    # Finds the filter that maps the best and saves it's conesponding character
-    # note: filter_matrix is a numpy array of type int32
     for filter_matrix, mapped_string in FILTER_PAIRS:
+        # note: filter_matrix is a numpy array of type int32 so
+        # multiplied_output inherits the type int32 (not uint8)
         multiplied_output = img_region * filter_matrix
         filter_output: int = np.sum(multiplied_output)
 
@@ -53,14 +68,19 @@ def print_filter_output(img_region: NDArray[np.uint8]):
     print(min_filer_string, end="")
 
 
-def print_img(img: NDArray[np.uint8]):
+def print_grayscale_img(img: NDArray[np.uint8]) -> None:
+    """Prints a grayscale image as ASCII characters to console.
+
+    Args:
+        img: a numpy array of grayscale pixels (shape = (x, x, 3))
+    """
     for i in range(1, LOAD_IMG_HEIGHT - 1):
         for j in range(1, LOAD_IMG_WIDTH - 1):
-            # Loads the pixel at location i, j and the along with the 8
-            # surrounding it
+            # Loads a 9 pixel region of the image
             img_region = img[i - 1 : i + 2, j - 1 : j + 2]
 
-            # Load the pixels as a 3 * 3 array (one int representing each pixel)
+            # Load the pixels as a 3 * 3 array (one int representing the
+            # grayscale value of each pixel in the region)
             img_region = img_region[:, :, 0:1]
             img_region = img_region.reshape((3, 3))
 
@@ -68,8 +88,8 @@ def print_img(img: NDArray[np.uint8]):
 
 
 def main() -> None:
-    # Loads image as 80 * 80 pixels and saves it in a numpy array
-    img_raw = Image.open("github.png")
+    """Loads an image and prints it as ASCII characters to the console"""
+    img_raw = Image.open("example_image.png")
     img_raw = img_raw.resize((LOAD_IMG_WIDTH, LOAD_IMG_HEIGHT))
     img: NDArray[np.uint8] = np.array(img_raw)
 
@@ -77,11 +97,10 @@ def main() -> None:
     if img_raw.mode == "RGBA":
         img = img[:, :, 0:3]
 
-    # Changes image to grayscale
     np.apply_along_axis(change_pixel_to_gray_scale, axis=2, arr=img)
 
     print("*" * shutil.get_terminal_size().columns)
-    print_img(img)
+    print_grayscale_img(img)
     print("*" * shutil.get_terminal_size().columns)
 
 
